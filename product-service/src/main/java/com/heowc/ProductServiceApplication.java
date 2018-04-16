b4c16f84ada220f6924757f64329c8d7db8c3e1f
heowc <heowc1992@gmail.com>
2018-04-16 22:51:40 +0900

    add sample value

--- product-service/src/main/java/com/heowc/ProductServiceApplication.java
+++ product-service/src/main/java/com/heowc/ProductServiceApplication.java
@@ -1,12 +1,24 @@
 package com.heowc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+    @Value("${info.description}")
+    private String description;
+
+    @GetMapping("/")
+    public String getDescription() {
+        return description;
     }
 }
--- user-service/src/main/java/com/heowc/UserServiceApplication.java
+++ user-service/src/main/java/com/heowc/UserServiceApplication.java
@@ -1,12 +1,24 @@
 package com.heowc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Value("${info.description}")
+    private String description;
+
+    @GetMapping("/")
+    public String getDescription() {
+        return description;
     }
 }
